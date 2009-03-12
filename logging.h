3b87daef0d9e515ff285af2a860f36010176b176
Andreas Ericsson <ae@op5.se>
2009-03-12 14:25:32 +0100

    logging: Add plenty of debug information in logs
    For now at least, this is a good change as we're doing
    heavy development. This patch should be reverted when
    this app is ready for production use.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- logging.h
+++ logging.h
@@ -8,13 +8,13 @@
 #define ldebug(fmt, args...) \
 	log_msg(LOG_DEBUG, "%s:%s():%d: " fmt, __FILE__, __func__, __LINE__, ##args)
 #define linfo(fmt, args...) \
-	log_msg(LOG_INFO, fmt, ##args)
+	log_msg(LOG_INFO, "%s:%s():%d" fmt, __FILE__, __func__, __LINE__, ##args)
 #define lmsg(fmt, args...) \
-	log_msg(LOG_NOTICE, fmt, ##args)
+	log_msg(LOG_NOTICE, "%s:%s():%d" fmt, __FILE__, __func__, __LINE__, ##args)
 #define lwarn(fmt, args...) \
-	log_msg(LOG_WARNING, fmt, ##args)
+	log_msg(LOG_WARNING, "%s:%s():%d" fmt, __FILE__, __func__, __LINE__, ##args)
 #define lerr(fmt, args...) \
-	log_msg(LOG_ERR, fmt, ##args)
+	log_msg(LOG_ERR, "%s:%s():%d" fmt, __FILE__, __func__, __LINE__, ##args)
 
 extern int log_init(void);
 extern void log_deinit(void);
