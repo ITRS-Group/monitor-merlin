949cc844e0dcf05c51c1ece67191eb44c92cb6cd
Andreas Ericsson <ae@op5.se>
2009-03-18 15:30:26 +0100

    db_updater: Add query for inserting program status data
    Tactical overview needs this for its completion.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- db_updater.c
+++ db_updater.c
@@ -64,6 +64,44 @@ static int mdb_update_service_status(const nebstruct_service_check_data *p)
 	return result;
 }
 
+static int mdb_update_program_status(const nebstruct_program_status_data *p)
+{
+	char *global_host_event_handler;
+	char *global_service_event_handler;
+	int result;
+
+	ldebug("Updating program status data");
+	sql_quote(p->global_host_event_handler, &global_host_event_handler);
+	sql_quote(p->global_service_event_handler, &global_service_event_handler);
+
+	result = sql_query
+		("UPDATE monitor_gui.program_status SET is_running = 1, "
+		 "last_alive = %lu, program_start = %lu, pid = %d, daemon_mode = %d, "
+		 "last_command_check = %lu, last_log_rotation = %lu, "
+		 "notifications_enabled = %d, "
+		 "active_service_checks_enabled = %d, passive_service_checks_enabled = %d, "
+		 "active_host_checks_enabled = %d, passive_host_checks_enabled = %d, "
+		 "event_handlers_enabled = %d, flap_detection_enabled = %d, "
+		 "failure_prediction_enabled = %d, process_performance_data = %d, "
+		 "obsess_over_hosts = %d, obsess_over_services = %d, "
+		 "modified_host_attributes = %lu, modified_service_attributes = %lu, "
+		 "global_host_event_handler = %s, global_service_event_handler = %s",
+		 time(NULL), p->program_start, p->pid, p->daemon_mode,
+		 p->last_command_check, p->last_log_rotation,
+		 p->notifications_enabled,
+		 p->active_service_checks_enabled, p->passive_service_checks_enabled,
+		 p->active_host_checks_enabled, p->passive_host_checks_enabled,
+		 p->event_handlers_enabled, p->flap_detection_enabled,
+		 p->failure_prediction_enabled, p->process_performance_data,
+		 p->obsess_over_hosts, p->obsess_over_services,
+		 p->modified_host_attributes, p->modified_service_attributes,
+		 global_host_event_handler, global_service_event_handler);
+
+	free(global_host_event_handler);
+	free(global_service_event_handler);
+	return result;
+}
+
 int mrm_db_update(struct proto_pkt *pkt)
 {
 	int errors = 0;
@@ -80,6 +118,9 @@ int mrm_db_update(struct proto_pkt *pkt)
 
 	case NEBCALLBACK_SERVICE_CHECK_DATA:
 		errors = mdb_update_service_status((void *)pkt->body);
+		break;
+	case NEBCALLBACK_PROGRAM_STATUS_DATA:
+		errors = mdb_update_program_status((void *)pkt->body);
 		break;
 	default:
 		ldebug("Unknown callback type. Weird, to say the least...");
