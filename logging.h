3ad9a9b6de0f54eed471276c27b184ba5cb77a90
Andreas Ericsson <ae@op5.se>
2009-04-22 20:07:26 +0200

    logging.h: Pull in the important macros from syslog.h
    Apparently, syslog.h plays merry hell with splint and sparse,
    but with this minor change we can still use both those tools
    to check the code.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- logging.h
+++ logging.h
@@ -2,8 +2,15 @@
 #define LOGGING_H
 
 #include <stdarg.h>
-#include <syslog.h>
 
+#define	LOG_EMERG	0	/* system is unusable */
+#define	LOG_ALERT	1	/* action must be taken immediately */
+#define	LOG_CRIT	2	/* critical conditions */
+#define	LOG_ERR		3	/* error conditions */
+#define	LOG_WARNING	4	/* warning conditions */
+#define	LOG_NOTICE	5	/* normal but significant condition */
+#define	LOG_INFO	6	/* informational */
+#define	LOG_DEBUG	7	/* debug-level messages */
 
 #define ldebug(fmt, args...) \
 	log_msg(LOG_DEBUG, "%s:%s():%d: " fmt, __FILE__, __func__, __LINE__, ##args)
