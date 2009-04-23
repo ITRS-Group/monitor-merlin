fd016c2fbc9582baf108c32f1b704b66e0ea867d
Andreas Ericsson <ae@op5.se>
2009-04-23 13:27:17 +0200

    Add init-script
    Signed-off-by: Andreas Ericsson <ae@op5.se>

---
+++ init.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+# Start / stop the Merlin daemon
+#
+# chkconfig: 2345 99 01
+#
+### BEGIN INIT INFO
+# Provides: monitor
+# Required-Start: $local_fs $network $remote_fs
+# Required-Stop: $local_fs $network $remote_fs
+# Default-Start:  2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: start and stop the merlin daemon
+# Description: Merlin is an event-distribution system for Nagios
+### END INIT INFO
+
+
+prog=merlind
+BINDIR=@@DESTDIR@@
+CONFIG_FILE=@@DESTDIR@@/merlin.conf
+
+start ()
+{
+	"$BINDIR/$prog" -c "$CONFIG_FILE"
+}
+
+stop ()
+{
+	"$BINDIR/$prog" -k
+}
+
+case "$1" in
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	restart)
+		stop
+		start
+		;;
+esac
