972fc7f533f33bd62b938cff00880504f3a42163
Andreas Ericsson <ae@op5.se>
2009-06-09 18:36:50 +0200

    import.php: Learn to grok arguments
    Such as database parameters and locations of files. A (very) basic
    config file parser is also included now so we can potentially read
    the location of objects.cache (though it's not always written) and
    status.(sav|dat) from Nagios' own configuration, which should help
    new users quite a bit.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- import.php
+++ import.php
@@ -9,4 +9,88 @@ $imp->db_host = 'localhost';
 $imp->db_user = 'merlin';
 $imp->db_pass = 'merlin';
 $imp->db_database = 'merlin';
-$imp->import_objects_from_cache();
+
+if (PHP_SAPI !== 'cli') {
+	$msg = "This program must be run from the command-line version of PHP\n";
+	echo $msg;
+	die($msg);
+}
+
+$progname = basename($argv[0]);
+function usage($msg = false)
+{
+	global $progname;
+
+	echo "Usage: $progname <options> --cache=/path/to/objects.cache\n";
+	echo "\n";
+	echo "--dbname   name of database to import to\n";
+	echo "--dbuser   database username\n";
+	echo "--dbhost   database host\n";
+	echo "--dbpass   database password\n";
+	echo "--dbtype   database type (mysql is the only supported for now)\n";
+	echo "--cache    path to the objects.cache file to import\n";
+	exit(1);
+}
+
+function read_nagios_cfg($cfgfile)
+{
+	if (!file_exists($cfgfile))
+		return false;
+
+	$cfg = false;
+	$lines = explode("\n", file_get_contents($cfgfile));
+	foreach ($lines as $line) {
+		$line = trim($line);
+		if (!strlen($line) || $line{0} === '#')
+			continue;
+		$ary = explode('=', $line);
+		$k = $ary[0];
+		$v = $ary[1];
+		if (isset($cfg[$k])) {
+			if (!is_array($cfg[$k]))
+				$cfg[$k] = array($cfg[$k]);
+			$cfg[$k][] = $v;
+			continue;
+		}
+		$cfg[$k] = $v;
+	}
+
+	return $cfg;
+}
+
+$cache = false;
+if (PHP_SAPI === 'cli' && $argc > 1) {
+	for ($i = 0; $i < $argc; $i++) {
+		$arg = $argv[$i];
+		if (substr($arg, 0, 2) !== '--') {
+			$cache = $arg;
+			continue;
+		}
+		$arg = substr($arg, 2);
+
+		$opt = false;
+		$optpos = strpos($arg, '=');
+		if ($optpos !== false) {
+			# must set 'opt' first
+			$opt = substr($arg, $optpos + 1);
+			$arg = substr($arg, 0, $optpos);
+		} elseif ($i < $argc - 1) {
+			$opt = $argc[++$i];
+		} else {
+			usage("$arg requires an option\n");
+		}
+		switch ($arg) {
+		 case 'dbname': $imp->db_database = $opt; break;
+		 case 'dbuser': $imp->db_user = $opt; break;
+		 case 'dbpass': $imp->db_pass = $opt; break;
+		 case 'dbtype': $imp->db_type = $opt; break;
+		 case 'dbhost': $imp->db_host = $opt; break;
+		 case 'cache': $cache = $opt; break;
+		 default:
+			usage("Unknown argument: $arg\n");
+			break;
+		}
+	}
+}
+echo "importing objects from $cache\n";
+$imp->import_objects_from_cache($cache);
