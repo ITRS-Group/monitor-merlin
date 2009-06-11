f8e1689c46543d617f3620ab0a58775932288b8e
Andreas Ericsson <ae@op5.se>
2009-06-11 02:18:07 +0200

    import.php: Grok --nagios-cfg and --status-log as arguments
    Since we will sometimes need to prime the database with configuration
    and status data before we can prime our own status tables, the import
    program must support importing from first objects.cache (since we
    really do need host->parents relationships in the database) and
    status.log all in one go.
    
    This patch makes it do just that.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- import.php
+++ import.php
@@ -58,39 +58,62 @@ function read_nagios_cfg($cfgfile)
 	return $cfg;
 }
 
-$cache = false;
-if (PHP_SAPI === 'cli' && $argc > 1) {
-	for ($i = 0; $i < $argc; $i++) {
-		$arg = $argv[$i];
-		if (substr($arg, 0, 2) !== '--') {
-			$cache = $arg;
-			continue;
-		}
-		$arg = substr($arg, 2);
+if (PHP_SAPI !== 'cli') {
+	usage("This program can only be run from the command-line\n");
+}
 
-		$opt = false;
-		$optpos = strpos($arg, '=');
-		if ($optpos !== false) {
-			# must set 'opt' first
-			$opt = substr($arg, $optpos + 1);
-			$arg = substr($arg, 0, $optpos);
-		} elseif ($i < $argc - 1) {
-			$opt = $argc[++$i];
-		} else {
-			usage("$arg requires an option\n");
-		}
-		switch ($arg) {
-		 case 'dbname': $imp->db_database = $opt; break;
-		 case 'dbuser': $imp->db_user = $opt; break;
-		 case 'dbpass': $imp->db_pass = $opt; break;
-		 case 'dbtype': $imp->db_type = $opt; break;
-		 case 'dbhost': $imp->db_host = $opt; break;
-		 case 'cache': $cache = $opt; break;
-		 default:
-			usage("Unknown argument: $arg\n");
-			break;
-		}
+$dry_run = $cache = $status_log = false;
+for ($i = 1; $i < $argc; $i++) {
+	$arg = $argv[$i];
+	if (substr($arg, 0, 2) !== '--') {
+		$cache = $arg;
+		continue;
+	}
+	$arg = substr($arg, 2);
+
+	$opt = false;
+	$optpos = strpos($arg, '=');
+	if ($optpos !== false) {
+		# must set 'opt' first
+		$opt = substr($arg, $optpos + 1);
+		$arg = substr($arg, 0, $optpos);
+	} elseif ($i < $argc - 1) {
+		$opt = $argc[++$i];
+	} elseif ($arg !== 'dry-run') {
+		usage("$arg requires an option\n");
+	}
+	switch ($arg) {
+	 case 'dbname': $imp->db_database = $opt; break;
+	 case 'dbuser': $imp->db_user = $opt; break;
+	 case 'dbpass': $imp->db_pass = $opt; break;
+	 case 'dbtype': $imp->db_type = $opt; break;
+	 case 'dbhost': $imp->db_host = $opt; break;
+	 case 'cache': $cache = $opt; break;
+	 case 'status-log': $status_log = $opt; break;
+	 case 'nagios-cfg': $nagios_cfg = $opt; break;
+	 case 'dry-run': $dry_run = true; break;
+	 default:
+		usage("Unknown argument: $arg\n");
+		break;
 	}
 }
-echo "importing objects from $cache\n";
-$imp->import_objects_from_cache($cache);
+
+if ($cache)
+	echo "importing objects from $cache\n";
+
+if (!$dry_run)
+	$imp->import_objects_from_cache($cache);
+
+if ($nagios_cfg && !$status_log) {
+	$config = read_nagios_cfg($nagios_cfg);
+	if (isset($config['status_file']))
+		$status_log = $config['status_file'];
+	elseif (isset($config['xsddefault_status_file']))
+		$status_log = $config['xsddefault_status_file'];
+}
+
+if ($status_log) {
+	echo "importing status from $status_log\n";
+	if (!$dry_run)
+		$imp->import_objects_from_cache($status_log);
+}
