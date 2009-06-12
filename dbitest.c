26f53f529b77bf35c13179b3b2bdc6832352e4c5
Andreas Ericsson <ae@op5.se>
2009-06-12 19:27:06 +0200

    dbitest.c: Test the binary search algorithm
    This test has already unearthed two bugs, so it's a good idea
    to keep it around. Especially if someone comes around later and
    wants to start optimizing this (already very fast) search.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- dbitest.c
+++ dbitest.c
@@ -5,6 +5,21 @@
 
 #define die(fmt, args...) do { fprintf(stderr, fmt, ##args); exit(1); } while(0)
 
+static size_t ok, id;
+int try_states(object_state *orig)
+{
+	object_state *st = get_object_state(orig->name, id);
+
+	if (st != orig) {
+		printf("Failed to locate item '%s'\n", orig->name);
+		printf("ok lookups: %d\n", ok);
+		exit(1);
+	}
+	ok++;
+
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
 	size_t hosts, services;
@@ -24,6 +39,11 @@ int main(int argc, char **argv)
 			sleep(1);
 	}
 	printf("Primed status for %zu hosts and %zu services\n", hosts, services);
+	foreach_state(0, try_states);
+	printf("%zu of %zu hosts located ok\n", ok, hosts);
+	ok = 0; id = 1;
+	foreach_state(1, try_states);
+	printf("%zu of %zu services located ok\n", ok, services);
 
 	return 0;
 }
