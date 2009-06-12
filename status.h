de2a6268273bfd7100e4d5bdeadbf542645fd447
Andreas Ericsson <ae@op5.se>
2009-06-12 19:44:45 +0200

    status.h: Add some handy macros to handle state and state types
    Since 'state' is stored as a single variable in memory, we need
    to do some binary operations on it to extract the state and the
    type from that variable. It's also handy to have a macro to
    concatenate the state, so that it's done the same everywhere
    we do it. This patch introduces those macros.
    
    Note that it doesn't actually make sense to store the variables
    separate, since we'll need to compare the concatenated state
    once we have the reports-module merged into Merlin anyway, and
    this way will be slightly faster since the integer we're working
    with can be loaded into a single register, as opposed to hitting
    memory twice.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- status.h
+++ status.h
@@ -7,6 +7,11 @@ struct object_state {
 };
 typedef struct object_state object_state;
 
+/* macros used on the 'state' member of the object_state struct */
+#define extract_state(st) (state & 0xffff)
+#define extract_type(state) ((state >> 16) & 1)
+#define concat_state(type, state) ((type << 16) | state)
+
 extern int prime_object_states(size_t *hosts, size_t *services);
 extern object_state *get_host_state(const char *name);
 extern object_state *get_service_state(const char *h_name, const char *s_name);
