dcdde9ab22b58f6417738c67635841bb9d9b6b7d
Andreas Ericsson <ae@op5.se>
2010-03-22 10:41:21 +0100

    Add missing header-file "colors.h"
    This wasn't added when bltest.c was modified to include it, so we do so
    now.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

---
+++ colors.h
@@ -0,0 +1,18 @@
+#ifndef colors_h__
+#define colors_h__
+#define CLR_RESET          "\033[m"
+#define CLR_BOLD           "\033[1m"
+#define CLR_RED            "\033[31m"
+#define CLR_GREEN          "\033[32m"
+#define CLR_BROWN          "\033[33m"
+#define CLR_YELLOW         "\033[33m\033[1m"
+#define CLR_BLUE           "\033[34m"
+#define CLR_MAGENTA        "\033[35m"
+#define CLR_CYAN           "\033[36m"
+#define CLR_BG_RED         "\033[41m"
+#define CLR_BRIGHT_RED     "\033[31m\033[1m"
+#define CLR_BRIGHT_GREEN   "\033[32m\033[1m"
+#define CLR_BRIGHT_BLUE    "\033[34m\033[1m"
+#define CLR_BRIGHT_MAGENTA "\033[35m\033[1m"
+#define CLR_BRIGHT_CYAN    "\033[36m\033[1m"
+#endif
