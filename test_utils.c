6167496817a0f807054627f70dfa66951b54e7c3
Andreas Ericsson <ae@op5.se>
2010-03-26 12:23:45 +0100

    test_utils: Flesh this out quite a bit
    We introduce a bunch of new functions that will help us print
    test-results prettily.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- test_utils.c
+++ test_utils.c
@@ -1,4 +1,73 @@
 #include "shared.h"
+#include "colors.h"
+const char *red = "", *green = "", *yellow = "", *reset = "";
+uint passed, failed;
+
+void t_set_colors(int force)
+{
+	if (force == 1 || (!force && isatty(fileno(stdout)))) {
+		red = CLR_RED;
+		yellow = CLR_YELLOW;
+		green = CLR_GREEN;
+		reset = CLR_RESET;
+	}
+}
+
+void t_pass(const char *name)
+{
+	passed++;
+	printf("%sPASS%s %s\n", green, reset, name);
+}
+
+void t_fail(const char *name)
+{
+	failed++;
+	printf("%sFAIL%s %s\n", red, reset, name);
+}
+
+void t_diag(const char *fmt, ...)
+{
+	if (!fmt) {
+		va_list ap;
+		putchar('\t');
+		va_start(ap, fmt);
+		vfprintf(stdout, fmt, ap);
+		va_end(ap);
+		putchar('\n');
+	}
+}
+
+void ok_int(int a, int b, const char *name)
+{
+	if (a == b)
+		t_pass(name);
+	else {
+		t_fail(name);
+		t_diag("%d != %d", a, b);
+	}
+}
+
+void ok_uint(uint a, uint b, const char *name)
+{
+	if (a == b)
+		t_pass(name);
+	else {
+		t_fail(name);
+		t_diag("%u != %d", a, b);
+	}
+}
+
+void __attribute__((__noreturn__)) crash(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	fputc('\n', stderr);
+
+	exit(1);
+}
 
 void log_msg(int severity, const char *fmt, ...)
 {
---
+++ test_utils.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_test_utils_h__
+#define INCLUDE_test_utils_h__
+extern const char *red, *green, *yellow, *reset;
+extern uint passed, failed;
+extern void t_set_colors(int force);
+extern void t_pass(const char *name);
+extern void t_fail(const char *name);
+extern void t_diag(const char *fmt, ...)
+	__attribute__((__format__(__printf__, 1, 2)));
+extern void ok_int(int a, int b, const char *name);
+extern void ok_uint(uint a, uint b, const char *name);
+extern void crash(const char *fmt, ...)
+	__attribute__((__format__(__printf__, 1, 2), __noreturn__));
+#endif
