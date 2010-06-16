c7d485ad0cf09ea8e0c4c30bcd65a4eb63980881
Andreas Ericsson <ae@op5.se>
2010-06-16 02:15:50 +0200

    io: Be as patient when sending as when reading
    In practice, we never seem to wait the maximum 1.5 seconds,
    so it's probably safe to do things like this. Hopefully it
    will prevent the few disconnects we still see which most
    likely happen in case we manage to empty a partially filled
    socket before the sending end has time to notice that it's
    writable and populate it with more data.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- io.c
+++ io.c
@@ -16,7 +16,7 @@ int io_poll(int fd, int events, int msec)
 
 int io_send_all(int fd, const void *buf, size_t len)
 {
-	int poll_ret, sent;
+	int poll_ret, sent, loops = 0;
 	size_t total = 0;
 
 	if (!buf || !len)
@@ -27,22 +27,25 @@ int io_send_all(int fd, const void *buf, size_t len)
 		lerr("io_poll(%d, POLLOUT, 0) returned %d: %s", fd, poll_ret, strerror(errno));
 
 	do {
+		loops++;
 		sent = send(fd, buf + total, len - total, MSG_DONTWAIT | MSG_NOSIGNAL);
 		if (poll_ret > 0 && sent + total == 0) {
 			/* disconnected peer? */
 			return 0;
 		}
 		if (sent < 0) {
+			if (errno == EAGAIN || errno == EWOULDBLOCK) {
+				sent = io_write_ok(fd, 100);
+				continue;
+			}
+
 			lerr("send(%d, (buf + total), %zu, MSG_DONTWAIT | MSG_NOSIGNAL) returned %d (%s)",
 				 fd, len - total, sent, strerror(errno));
-			if (errno != EAGAIN)
-				return sent;
-
 			continue;
 		}
 
 		total += sent;
-	} while (total < len && sent > 0);
+	} while (total < len && sent > 0 && loops < 15);
 
 	if (sent < 0)
 		return sent;
