23af180a01548af6aad1b5c3e9ffd991080190a1
Andreas Ericsson <ae@op5.se>
2010-06-18 13:05:36 +0200

    import: Ignore 2d_coords
    We already ignore 3d_coords, and postgresql doesn't support
    columns starting with a digit.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- object_importer.inc.php
+++ object_importer.inc.php
@@ -423,8 +423,11 @@ class nagios_object_importer
 			return $this->convert[$obj_type][$k];
 
 		if ($obj_type === 'host') {
-			if ($k === 'vrml_image' || $k === '3d_coords')
+			if ($k === 'vrml_image' || $k === '3d_coords' ||
+				$k === '2d_coords')
+			{
 				return false;
+			}
 		} elseif ($obj_type === 'program_status') {
 			if (substr($k, 0, strlen('enable_')) === 'enable_') {
 				return substr($k, strlen('enable_')) . '_enabled';
