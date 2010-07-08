baf289a16351b1e5333154111d25e0e7bdb90846
Andreas Ericsson <ae@op5.se>
2010-07-08 11:51:40 +0200

    test_utils.h: spell out unsigned int instead of using uint
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- test_utils.h
+++ test_utils.h
@@ -3,9 +3,9 @@
 #define TEST_PASS 0
 #define TEST_FAIL 1
 
-typedef unsigned int uint;
 extern const char *red, *green, *yellow, *cyan, *reset;
-extern uint passed, failed, t_verbose;
+extern unsigned int passed, failed, t_verbose;
+
 extern void t_set_colors(int force);
 extern void t_start(const char *fmt, ...)
 	__attribute__((__format__(__printf__, 1, 2)));
@@ -16,7 +16,7 @@ extern void t_fail(const char *fmt, ...)
 extern void t_diag(const char *fmt, ...)
 	__attribute__((__format__(__printf__, 1, 2)));
 extern int ok_int(int a, int b, const char *name);
-extern int ok_uint(uint a, uint b, const char *name);
+extern int ok_uint(unsigned int a, unsigned int b, const char *name);
 extern int ok_str(const char *a, const char *b, const char *name);
 extern int t_end(void);
 extern void crash(const char *fmt, ...)
