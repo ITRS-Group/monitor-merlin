d73ec17ee7ef56da5598e459e96562524e62b118
Andreas Ericsson <ae@op5.se>
2010-10-27 16:37:54 +0200

    showlog::auth.h: Remove a dead prototype
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- auth.h
+++ auth.h
@@ -5,7 +5,6 @@ extern int auth_service_ok(const char *host, const char *service);
 extern void auth_set_user(const char *username);
 extern void auth_parse_permission(const char *key, const char *value);
 extern const char *auth_get_user(void);
-extern void auth_parse_perm(const char *key, const char *value);
 extern int auth_init(const char *path);
 extern void auth_deinit(void);
 #endif
