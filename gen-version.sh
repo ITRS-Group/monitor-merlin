d3b018fee7d39d47b1092e42e35d859390459a90
Andreas Ericsson <ae@op5.se>
2010-12-10 16:32:15 +0100

    gen-version: Make the script portable
    With this patch it runs just fine on Solaris with straight old
    /bin/sh. It's one of those things we should probably test every
    now and then.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- gen-version.sh
+++ gen-version.sh
@@ -6,22 +6,22 @@ revision= patches= version= ver= beta= dirty=
 # First see if there is a version file (included in release tarballs),
 # then try git-describe, then default.
 if test -f version; then
-	ver=$(cat version) || ver="$DEF_VER"
+	ver=`cat version` || ver="$DEF_VER"
 elif test -d .git -o -f .git; then
-	ver=$(git describe --abbrev=12 HEAD 2>/dev/null)
-	tag=$(git describe --abbrev=0 HEAD 2>/dev/null)
+	ver=`git describe --abbrev=12 HEAD 2>/dev/null`
+	tag=`git describe --abbrev=0 HEAD 2>/dev/null`
 	case "$ver" in
 	v[0-9]*)
 		git update-index -q --refresh >/dev/null 2>&1
-		test -z "$(git diff-index --name-only HEAD --)" ||
+		test -z "`git diff-index --name-only HEAD --`" ||
 		dirty="-dirty"
 	esac
-	if ! test "$ver" = "$tag"; then
-		beta=$(expr "$ver" : .*[0-9]-'\(beta[0-9]*\)')
-		revision=$(expr "$ver" : '.*\(-g[a-f0-9]*\)$')
-		version=$(expr "$ver" : '\(.*\)-g[a-f0-9*]')
-		patches=p$(expr "$ver" : $tag-'\(.*\)'$revision)
-		ver=$(expr "$ver" : v*'\(.*\)')$dirty
+	if test "$ver" != "$tag"; then
+		beta=`expr "$ver" : .*[0-9]-'\(beta[0-9]*\)'`
+		revision=`expr "$ver" : '.*\(-g[a-f0-9]*\)$'`
+		version=`expr "$ver" : '\(.*\)-g[a-f0-9*]'`
+		patches=p`expr "$ver" : $tag-'\(.*\)'$revision`
+		ver=`expr "$ver" : v*'\(.*\)'`$dirty
 	fi
 	version="$tag$patches$revision$dirty"
 else
