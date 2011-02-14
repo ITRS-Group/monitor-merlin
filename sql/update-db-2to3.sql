ecdfcea329c4d609e0e45b7e257312703808b2d8
Andreas Ericsson <ae@op5.se>
2011-02-14 10:29:37 +0100

    2to3 sql update script
    Set version to 3 when we're done.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- sql/update-db-2to3.sql
+++ sql/update-db-2to3.sql
@@ -5,4 +5,4 @@ ALTER TABLE host
 ALTER TABLE service
 	CHANGE check_flapping_recovery_notification check_flapping_recovery_notifi int(10) NOT NULL default '0';
 
-UPDATE db_version SET version = 2;
+UPDATE db_version SET version = 3;
