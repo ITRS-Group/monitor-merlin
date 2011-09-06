ITRS-Group/monitor-merlin

commit 3177e9cd706509ec4699952900f7ecf0dc4619c0

app module merlin_status: Use instance id to detect local node
Previously we used a self-invented name which happened to change.
A lot depends on the local node always being instance id 0 though,
so we rely on that here too.

Signed-off-by: Andreas Ericsson <ae@op5.se>

apps/libexec/modules/merlin_status.py
@@ -241,8 +241,8 @@ class merlin_status:
 
 		nodes = {}
 		for row in self.dbc.fetchall():
-			name = row[1]
-			if name == 'Local Nagios/Merlin instance':
+			instance_id = row[0]
+			if instance_id == 0:
 				name = '_local.ipc'
 			nodes[name] = {}
 			res = {'iid': row[0], 'name': name, 'last_alive': row[2], 'active': row[3]}

commit c543e6fab34d4ae1f0bce4531fa22335ccc18890

app module merlin_status: Use instance name from database
This got broken in 3177e9cd, when I accidentally made any instance
with id 0 override all others.

Signed-off-by: Andreas Ericsson <ae@op5.se>

apps/libexec/modules/merlin_status.py
@@ -241,9 +241,11 @@ class merlin_status:
 
 		nodes = {}
 		for row in self.dbc.fetchall():
-			instance_id = row[0]
+			instance_id = int(row[0])
 			if instance_id == 0:
 				name = '_local.ipc'
+			else:
+				name = row[1]
 			nodes[name] = {}
 			res = {'iid': row[0], 'name': name, 'last_alive': row[2], 'active': row[3]}
 			res['self_assigned_peer_id'] = row[4]