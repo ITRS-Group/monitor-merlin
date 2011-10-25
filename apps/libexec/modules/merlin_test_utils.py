4978e4272cb84f3398f7ea11ec6526e21a8a4941
Andreas Ericsson <ae@op5.se>
2011-10-25 11:39:50 +0200

    module merlin_test_utils: Move pidfile to merlin subdirectory
    It just gets in the way in the root directory.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- apps/libexec/modules/merlin_test_utils.py
+++ apps/libexec/modules/merlin_test_utils.py
@@ -121,7 +121,7 @@ module {
 }
 
 daemon {
-	pidfile = @@DIR@@/merlin.pid;
+	pidfile = @@DIR@@/merlin/pidfile;
 	log_file = @@DIR@@/daemon.log;
 	import_program = @@OCIMP_PATH@@
 	port = @@NETWORK_PORT@@;
