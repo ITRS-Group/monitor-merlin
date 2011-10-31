d2e2603b3df839a8c5c145e7d86d30522c6f9f35
Robin Sonefors <robin.sonefors@op5.com>
2011-10-31 15:15:24 +0100

    sql/oracle: report_data.id doesn't exist, so don't create trigger for it
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- sql/oracle/merlin_seq.sql
+++ sql/oracle/merlin_seq.sql
@@ -1707,34 +1707,6 @@ END;
 
 /
 
-CREATE OR REPLACE TRIGGER report_data_id_TRG BEFORE INSERT OR UPDATE ON report_data
-FOR EACH ROW
-DECLARE
-v_newVal NUMBER(12) := 0;
-v_incval NUMBER(12) := 0;
-BEGIN
-  IF INSERTING AND :new.id IS NULL THEN
-    SELECT  report_data_id_SEQ.NEXTVAL INTO v_newVal FROM DUAL;
-    -- If this is the first time this table have been inserted into (sequence == 1)
-    IF v_newVal = 1 THEN
-      --get the max indentity value from the table
-      SELECT NVL(max(id),0) INTO v_newVal FROM report_data;
-      v_newVal := v_newVal + 1;
-      --set the sequence to that value
-      LOOP
-           EXIT WHEN v_incval>=v_newVal;
-           SELECT report_data_id_SEQ.nextval INTO v_incval FROM dual;
-      END LOOP;
-    END IF;
-    --used to emulate LAST_INSERT_ID()
-    --mysql_utilities.identity := v_newVal;
-   -- assign the value from the sequence to emulate the identity column
-   :new.id := v_newVal;
-  END IF;
-END;
-
-/
-
 CREATE OR REPLACE TRIGGER contactgroup_id_TRG BEFORE INSERT OR UPDATE ON contactgroup
 FOR EACH ROW
 DECLARE
