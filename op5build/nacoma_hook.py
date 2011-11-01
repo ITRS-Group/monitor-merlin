67542650da25dc8318a0183c7bba2b4c780e770f
Robin Sonefors <robin.sonefors@op5.com>
2011-11-01 11:34:56 +0100

    nacoma_hook: Handle both oracle and mysql quoting styles
    Why create a quoting standard that specifies 6 different ways to quote
    strings and suggesting to driver authors that they should only support
    one of them each? Isn't that pretty much the opposite of standardizing?
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- op5build/nacoma_hook.py
+++ op5build/nacoma_hook.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import inspect
 from nacoma.hooks import Change
 reportable_types = ['host', 'service']
 
@@ -12,15 +13,28 @@ import merlin_db
 conn = merlin_db.connect(mconf)
 cursor = conn.cursor()
 
+paramstyle = inspect.getmodule(type(conn)).paramstyle
+
 for line in sys.stdin:
     change = Change(line)
     if change.type not in reportable_types:
         continue
     if change.is_renamed():
         if change.type == 'host':
-            cursor.execute('INSERT INTO rename_log(from_host_name, from_service_description, to_host_name, to_service_description) VALUES (%s, NULL, %s, NULL)', [change.oldname, change.newname])
+            arg = {'oldhost': change.oldname, 'newhost': change.newname}
+            if paramstyle == 'named':
+                query = 'INSERT INTO rename_log(from_host_name, from_service_description, to_host_name, to_service_description) VALUES (:oldhost, NULL, :newhost, NULL)'
+            elif paramstyle == 'format':
+                query = 'INSERT INTO rename_log(from_host_name, from_service_description, to_host_name, to_service_description) VALUES (%s, NULL, %s, NULL)'
+                arg = arg.values()
+            cursor.execute(query, arg)
         else:
-            cursor.execute('INSERT INTO rename_log(from_host_name, from_service_description, to_host_name, to_service_description) VALUES (%s, %s, %s, %s)', change.oldname.split(';') + change.newname.split(';'))
-
+            arg =  zip(('oldhost', 'oldservice', 'newhost', 'newservice'), change.oldname.split(';') + change.newname.split(';'))
+            if conn.paramstyle == 'named':
+                query = 'INSERT INTO rename_log(from_host_name, from_service_description, to_host_name, to_service_description) VALUES (:oldhost, :oldservice, :newhost, :newservice)'
+            elif conn.paramstyle == 'format':
+                query = 'INSERT INTO rename_log(from_host_name, from_service_description, to_host_name, to_service_description) VALUES (%s, %s, %s, %s)'
+                arg = arg.values()
+            cursor.execute(query, arg)
 conn.commit()
 conn.close()
