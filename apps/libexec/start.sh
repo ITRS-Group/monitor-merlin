6cea8eb64889df92403ec3c2770d893af53a04c7
Andreas Ericsson <ae@op5.se>
2012-04-10 13:09:41 +0200

    mon start: Handle synergy config clash when we have masters
    In case we have masters and synergy is installed on the system, we
    need to remove the synergy configuration from the poller, since it
    will be sent to us from the master.
    
    We handle it here by simply removing the cfg_dir line in nagios.cfg
    in case it exists. This should be simple enough that it doesn't
    mess things up for any opensource users.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- apps/libexec/start.sh
+++ apps/libexec/start.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+if mon node show | grep -q ^TYPE=master; then
+	# we have masters, so we're a poller
+	# Disable synergy nagios stuff (fugly workaround)
+	ncfg=/opt/monitor/etc/nagios.cfg
+	test -f $ncfg && sed -i '#^cfg_dir=/opt/monitor/etc/synergy#d' $ncfg
+fi
+
 /etc/init.d/merlind start
 /etc/init.d/monitor start
