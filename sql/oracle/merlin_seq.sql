2569d8423db6c1ce1b9847c15a19686cd9ac7f89
Robin Sonefors <robin.sonefors@op5.com>
2012-06-26 09:49:39 +0200

    oracle/merlin: Remove 5.6 SQL upgrades completely
    They were only partially applied, so remove them completely, and require
    the use of the upgrade script.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- sql/oracle/merlin_seq.sql
+++ sql/oracle/merlin_seq.sql
@@ -852,37 +852,7 @@ CREATE TABLE program_status (
   modified_service_attributes NUMBER(10,0),
   global_host_event_handler CLOB,
   global_service_event_handler CLOB,
-  peer_id NUMBER(10,0),
-  node_type NUMBER(6,0),
-  config_hash VARCHAR2(255 CHAR),
-  self_assigned_peer_id NUMBER(10,0),
-  active_peers NUMBER(10,0),
-  configured_peers NUMBER(10,0),
-  active_pollers NUMBER(10,0),
-  configured_pollers NUMBER(10,0),
-  active_masters NUMBER(10,0),
-  configured_masters NUMBER(10,0),
-  host_checks_handled NUMBER(10,0),
-  service_checks_handled NUMBER(10,0)
 );
-
-set term off;
-ALTER TABLE program_status ADD (
-  peer_id NUMBER(10,0),
-  node_type NUMBER(6,0),
-  config_hash VARCHAR2(255 CHAR),
-  self_assigned_peer_id NUMBER(10,0),
-  active_peers NUMBER(10,0),
-  configured_peers NUMBER(10,0),
-  active_pollers NUMBER(10,0),
-  configured_pollers NUMBER(10,0),
-  active_masters NUMBER(10,0),
-  configured_masters NUMBER(10,0),
-  host_checks_handled NUMBER(10,0),
-  service_checks_handled NUMBER(10,0)
-);
-set term on;
-
 
 
 
@@ -908,12 +878,9 @@ CREATE TABLE report_data (
   hard NUMBER(10,0) DEFAULT '0' NOT NULL,
   retry NUMBER(10,0) DEFAULT '0' NOT NULL,
   downtime_depth NUMBER(10,0),
-  output CLOB
+  output CLOB,
+  id NUMBER(10,0)
 );
-
-set term off
-ALTER TABLE report_data DROP COLUMN id;
-set term on
 
 
 PROMPT Creating Index rd_timestamp on report_data ...
