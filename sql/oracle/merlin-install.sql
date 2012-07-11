0d179b5210ea7a8187ebccfbc9cc6f51f1af467b
Robin Sonefors <robin.sonefors@op5.com>
2012-07-11 14:08:29 +0200

    oracle/sql: Refactor
    Create a monitor-5.4-to-v5.5 script - the base is still 5.5, though -
    and add a 5.6-to-5.7 script.
    
    Split merlin_seq into a merlin-always and a merlin-install - the former
    should "always" be used, and the latter is only run on every "install".
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

---
+++ sql/oracle/merlin-install.sql
@@ -0,0 +1,121 @@
+DROP SEQUENCE notification_id_SEQ;
+CREATE SEQUENCE  notification_id_SEQ
+  MINVALUE 1 MAXVALUE 999999999999999999999999 INCREMENT BY 1  NOCYCLE ;
+
+CREATE TABLE merlin_importer
+(
+  pid NUMBER(10,0) DEFAULT NULL
+);
+
+CREATE TABLE notification (
+  instance_id NUMBER(10,0) DEFAULT '0' NOT NULL,
+  id NUMBER(10,0) NOT NULL,
+  notification_type NUMBER(10,0),
+  start_time NUMBER(10,0),
+  end_time NUMBER(10,0),
+  contact_name VARCHAR2(255 CHAR),
+  host_name VARCHAR2(255 CHAR),
+  service_description VARCHAR2(255 CHAR),
+  command_name VARCHAR2(1024 CHAR),
+  reason_type NUMBER(10,0),
+  state NUMBER(10,0),
+  output CLOB,
+  ack_author VARCHAR2(255 CHAR),
+  ack_data CLOB,
+  escalated NUMBER(10,0),
+  contacts_notified NUMBER(10,0)
+);
+ALTER TABLE notification ADD CONSTRAINT PRIMARY_8 PRIMARY KEY(id) ENABLE;
+CREATE INDEX n_host_name ON notification(host_name);
+CREATE INDEX n_service_name ON notification(host_name, service_description);
+CREATE INDEX n_contact_name ON notification(contact_name);
+
+
+CREATE TABLE perfdata (
+  timestamp NUMBER(10,0) NOT NULL,
+  host_name VARCHAR2(70 CHAR) NOT NULL,
+  service_description VARCHAR2(200 CHAR),
+  perfdata CLOB NOT NULL
+);
+CREATE INDEX pd_time ON perfdata(timestamp);
+CREATE INDEX pd_host_name ON perfdata(host_name);
+CREATE INDEX pd_service_name ON perfdata(host_name, service_description);
+
+CREATE TABLE report_data (
+  timestamp NUMBER(10,0) DEFAULT '0' NOT NULL,
+  event_type NUMBER(10,0) DEFAULT '0' NOT NULL,
+  flags NUMBER(10,0),
+  attrib NUMBER(10,0),
+  host_name VARCHAR2(160 CHAR),
+  service_description VARCHAR2(160 CHAR),
+  state NUMBER(10,0) DEFAULT '0' NOT NULL,
+  hard NUMBER(10,0) DEFAULT '0' NOT NULL,
+  retry NUMBER(10,0) DEFAULT '0' NOT NULL,
+  downtime_depth NUMBER(10,0),
+  output CLOB,
+);
+CREATE INDEX rd_timestamp ON report_data(timestamp);
+CREATE INDEX rd_event_type ON report_data(event_type);
+CREATE INDEX rd_host_name ON report_data(host_name);
+CREATE INDEX rd_service_name ON report_data(host_name, service_description);
+CREATE INDEX rd_state ON report_data(state);
+
+CREATE TABLE rename_log (
+  id NUMBER(10,0) NOT NULL,
+  from_host_name VARCHAR2(255 CHAR),
+  from_service_description VARCHAR2(255 CHAR) DEFAULT NULL,
+  to_host_name VARCHAR2(255 CHAR),
+  to_service_description VARCHAR2(255 CHAR) DEFAULT NULL
+);
+ALTER TABLE rename_log ADD CONSTRAINT rename_log_pk PRIMARY KEY(id) ENABLE;
+
+
+CREATE OR REPLACE TRIGGER notification_id_TRG BEFORE INSERT OR UPDATE ON notification
+FOR EACH ROW
+DECLARE
+v_newVal NUMBER(12) := 0;
+v_incval NUMBER(12) := 0;
+BEGIN
+  IF INSERTING AND :new.id IS NULL THEN
+    SELECT  notification_id_SEQ.NEXTVAL INTO v_newVal FROM DUAL;
+    -- If this is the first time this table have been inserted into (sequence == 1)
+    IF v_newVal = 1 THEN
+      --get the max indentity value from the table
+      SELECT NVL(max(id),0) INTO v_newVal FROM notification;
+      v_newVal := v_newVal + 1;
+      --set the sequence to that value
+      LOOP
+           EXIT WHEN v_incval>=v_newVal;
+           SELECT notification_id_SEQ.nextval INTO v_incval FROM dual;
+      END LOOP;
+    END IF;
+    --used to emulate LAST_INSERT_ID()
+    --mysql_utilities.identity := v_newVal;
+   -- assign the value from the sequence to emulate the identity column
+   :new.id := v_newVal;
+  END IF;
+END;
+
+/
+
+CREATE OR REPLACE TRIGGER rename_log_id_TRG BEFORE INSERT OR UPDATE ON rename_log
+FOR EACH ROW
+DECLARE
+v_newVal NUMBER(12) := 0;
+v_incVal NUMBER(12) := 0;
+BEGIN
+  IF INSERTING AND :new.id IS NULL THEN
+    SELECT  rename_log_id_SEQ.NEXTVAL INTO v_newVal FROM DUAL;
+    IF v_newVal = 1 THEN
+      SELECT NVL(max(id),0) INTO v_newVal FROM rename_log;
+      v_newVal := v_newVal + 1;
+      LOOP
+        EXIT WHEN v_incval>=v_newVal;
+        SELECT rename_log_id_SEQ.nextval INTO v_incval FROM dual;
+      END LOOP;
+    END IF;
+    :new.id := v_newVal;
+  END IF;
+END;
+
+/
--- sql/oracle/v5.5_to_v5.6.sql
+++ sql/oracle/v5.5_to_v5.6.sql
@@ -1,17 +1,2 @@
 DROP INDEX rd_host_name ON report_data;
 CREATE INDEX rd_evt_service_name ON report_data(event_type, host_name, service_description);
-ALTER TABLE program_status ADD (
-  peer_id NUMBER(10,0),
-  node_type NUMBER(6,0),
-  config_hash VARCHAR2(255 CHAR),
-  self_assigned_peer_id NUMBER(10,0),
-  active_peers NUMBER(10,0),
-  configured_peers NUMBER(10,0),
-  active_pollers NUMBER(10,0),
-  configured_pollers NUMBER(10,0),
-  active_masters NUMBER(10,0),
-  configured_masters NUMBER(10,0),
-  host_checks_handled NUMBER(10,0),
-  service_checks_handled NUMBER(10,0)
-);
-ALTER TABLE report_data DROP COLUMN id;
