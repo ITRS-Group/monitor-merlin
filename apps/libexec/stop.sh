92dbd9c7c2e366801763533f3988c9e0eff52029
Carl Helmertz <chelmertz@op5.com>
2012-10-04 14:41:24 +0200

    solaris: Adapt mon-scripts
    Some of the sed's were quite alright but we don't, for example, have -q
    for grep in the Solaris' builtin. As such, mon start had syntax errors.
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- apps/libexec/stop.sh
+++ apps/libexec/stop.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-/etc/init.d/monitor stop
-/etc/init.d/monitor slay
-/etc/init.d/merlind stop
+if [ "`uname`" = "SunOS" ]; then
+	/opt/monitor/svc/svc-monitor slay
+	svcadm disable -t op5monitor
+	svcadm disable -ts op5merlin
+else
+	/etc/init.d/monitor stop
+	/etc/init.d/monitor slay
+	/etc/init.d/merlind stop
+fi
