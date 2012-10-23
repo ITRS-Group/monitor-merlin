02771ccbd4947d3c8761dbfb0942f6bdf9f3c047
Andreas Ericsson <ae@op5.se>
2012-10-23 13:53:05 +0200

    module: Unload post_config_init() so we only run it once
    It's a minor thing really, but it will save a few cycles, get rid
    of a bit of code and also remove a TODO comment. Win, win and win.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- module.c
+++ module.c
@@ -798,9 +798,7 @@ static int post_config_init(int cb, void *ds)
 		return 0;
 
 	/* only call this function once */
-	/* We can't deregister our own call back at this time, due to a bug
-           in Nagios.  Removing the deregister until such time as it is safe: */
-	/* neb_deregister_callback(NEBCALLBACK_PROCESS_DATA, post_config_init); */
+	neb_deregister_callback(NEBCALLBACK_PROCESS_DATA, post_config_init);
 
 	linfo("Object configuration parsed.");
 	ctrl_create_object_tables();
