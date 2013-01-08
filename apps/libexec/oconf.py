ITRS-Group/monitor-merlin

commit 0ab0721f0743ed955b29d8a9b0a766393dcfff1c

mon oconf: Support relative paths for object config files
Nagios supports nagios.cfg-relative paths for object config files,
so we should too. This makes "cfg_file=lala.cfg" work properly
instead of forcing it to look in /opt/monitor/etc for the file.

Signed-off-by: Andreas Ericsson <ae@op5.se>

apps/libexec/oconf.py
@@ -56,13 +56,16 @@ parse_order = [
 	]
 
 # grab object configuration files from a cfg_dir directive
-def recurse_grab_object_cfg_files(v):
+def recurse_grab_object_cfg_files(v, basepath=''):
 	obj_files = []
-	f_ary = os.listdir(v)
+	if v[0] == '/':
+		f_ary = os.listdir(v)
+	else:
+		f_ary = os.listdir(basepath + '/' + v)
 	for f in f_ary:
-		path = v + '/' + f
+		path = '%s%s/%s' % (basepath, v, f)
 		if os.path.isdir(path):
-			obj_files += recurse_grab_object_cfg_files(path)
+			obj_files += recurse_grab_object_cfg_files(path, '')
 		elif os.path.isfile(path) and f[-4:] == '.cfg':
 			obj_files.append(path)
 
@@ -73,11 +76,15 @@ def grab_nagios_cfg(nagios_cfg_path):
 
 	obj_files = []
 	comp = parse_conf(nagios_cfg_path)
+	basepath = os.path.dirname(os.path.abspath(nagios_cfg_path))
 	for (k, v) in comp.params:
 		if k == 'cfg_file':
-			obj_files.append(v)
+			if v[0] == '/':
+				obj_files.append(v)
+			else:
+				obj_files.append(basepath + '/' + v)
 		elif k == 'cfg_dir':
-			obj_files += recurse_grab_object_cfg_files(v)
+			obj_files += recurse_grab_object_cfg_files(v, basepath + '/')
 		elif k.endswith("_command"):
 			force_include_commands[k] = v;
 

commit b6fcc4ffbac118ec4a8fdf021c9ee9aa58a2b15f

mon oconf: Fix parsing absolute cfg_dir statements
This was broken in 0ab0721f0743ed955b29d8a9b0a766393dcfff1c, which
introduced support for relative paths for object configuration files
in the 'mon oconf' suite, but with a botched implementation for
absolute cfg_dir paths.

Not so hot, but this patch fixes it.

Signed-off-by: Andreas Ericsson <ae@op5.se>

apps/libexec/oconf.py
@@ -58,10 +58,7 @@ parse_order = [
 # grab object configuration files from a cfg_dir directive
 def recurse_grab_object_cfg_files(v, basepath=''):
 	obj_files = []
-	if v[0] == '/':
-		f_ary = os.listdir(v)
-	else:
-		f_ary = os.listdir(basepath + '/' + v)
+	f_ary = os.listdir(basepath + '/' + v)
 	for f in f_ary:
 		path = '%s%s/%s' % (basepath, v, f)
 		if os.path.isdir(path):
@@ -84,7 +81,10 @@ def grab_nagios_cfg(nagios_cfg_path):
 			else:
 				obj_files.append(basepath + '/' + v)
 		elif k == 'cfg_dir':
-			obj_files += recurse_grab_object_cfg_files(v, basepath + '/')
+			if v[0] == '/':
+				obj_files += recurse_grab_object_cfg_files(v, '')
+			else:
+				obj_files += recurse_grab_object_cfg_files(v, basepath + '/')
 		elif k.endswith("_command"):
 			force_include_commands[k] = v;
 