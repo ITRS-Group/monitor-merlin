24126d7c0fcb061cd8c87d12e1c693e0b8c77f6d
Robin Sonefors <robin.sonefors@op5.com>
2013-02-14 11:05:32 +0100

    report-data-indexes: Update indexes
    Testing reveals that this makes the initial state/downtime depth
    queries in ninja awesomely quick, rather than stupidly slow. Awesomely
    quick is awesome.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- sql/mysql/report-data-indexes.sql
+++ sql/mysql/report-data-indexes.sql
@@ -1,5 +1,4 @@
 CREATE INDEX rd_timestamp ON report_data(timestamp);
 CREATE INDEX rd_event_type ON report_data(event_type);
-CREATE INDEX rd_service_name ON report_data(host_name, service_description);
-CREATE INDEX rd_evt_service_name ON report_data(event_type, host_name, service_description);
+CREATE INDEX rd_name_evt_time ON report_data(host_name,service_description,event_type,hard,timestamp);
 CREATE INDEX rd_state ON report_data(state);
