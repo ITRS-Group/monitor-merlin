c5769abe2ce270d63a641a101f1ce087649f91d1
Anton Lofgren <alofgren@op5.com>
2013-05-28 11:30:21 +0200

    test: Add more wireframe and dummy data
    We should now be able to start writing some tests by decoding the
    packages that arrive at the ipc_send_event function. This allows us to
    verify that any transformations/mutations done to objects are correct.
    Of course, it also enables checking that mutations are NOT done we don't
    want them to be.
    
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

--- test-hooks.c
+++ test-hooks.c
@@ -2,18 +2,29 @@
 #include "test_utils.h"
 
 #define T_ASSERT(pred, msg) do {\
-	if ((pred)) { t_pass(msg); } else { t_fail(msg); } \
+	if ((pred)) { t_pass(msg); } else { t_fail(msg); exit(1); } \
 	} while (0)
 /* extern STUBS */
 merlin_node *merlin_sender = NULL;
+merlin_node ipc;
 struct merlin_notify_stats merlin_notify_stats[9][2][2];
 time_t merlin_should_send_paths = 1;
 void *neb_handle = NULL;
 
 void set_host_check_node(merlin_node *node, host *h) {}
+node_selection *node_selection_by_name(const char *name){ return NULL; }
 node_selection *node_selection_by_hostname(const char *name){ return NULL; }
 void set_service_check_node(merlin_node *node, service *s) {}
+int ipc_send_event(merlin_event *pkt) {
+	printf("stub called!\n");
+	return 0;
+}
+int ipc_grok_var(char *var, char *val) {return 0;}
 int send_paths(void) { return 0; }
+
+int node_ctrl(merlin_node *node, int code, uint selection, void *data, uint32_t len, int msec) {
+	return 0;
+}
 
 int neb_register_callback(int callback_type, void *mod_handle, int priority, int (*callback_func)(int, void *)) {
 	return 0;
@@ -25,15 +36,19 @@ int neb_deregister_callback(int callback_type, int (*callback_func)(int, void *)
 /* extern STUBS */
 
 void test_callback_host_status() {
-	host *hst =NULL;
-	nebstruct_host_check_data ev_data;
+	host hst;
+	memset(&hst, 0, sizeof(host));
+	hst.id = 1;
+	hst.name = "test-host";
+	/*time_t expected_last_check = time(NULL);*/
+	nebstruct_host_status_data ev_data;
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
 	ev_data.type = NEBTYPE_HOSTSTATUS_UPDATE;
 	ev_data.flags = 0;
 	ev_data.attr = 0;
 	ev_data.timestamp = tv;
-	ev_data.object_ptr = (void *)hst;
+	ev_data.object_ptr = &hst;
 	merlin_mod_hook(NEBCALLBACK_HOST_STATUS_DATA, &ev_data);
 	T_ASSERT(0 == 0, "zero should equal itself!");
 }
@@ -44,4 +59,5 @@ int main(int argc, char *argv[]) {
 
 	t_start("testing neb module to daemon interface");
 	test_callback_host_status();
+	return 0;
 }
