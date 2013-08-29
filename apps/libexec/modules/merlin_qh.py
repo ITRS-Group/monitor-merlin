ca8ba3612a980a50ab0ba4a1f15150779f607d4d
Andreas Ericsson <ae@op5.se>
2013-08-29 10:36:02 +0200

    mon module merlin_qh: Sort nodeinfo before returning it
    This makes it a lot more pleasant to compare output from different
    nodes where the hashing happens to end up in different order than
    on a peer node.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- apps/libexec/modules/merlin_qh.py
+++ apps/libexec/modules/merlin_qh.py
@@ -1,5 +1,16 @@
 import nagios_qh
 
+def nodeinfo_sorter(a, b):
+	if a['name'] > b['name']:
+		return 1
+	if b['name'] > a['name']:
+		return -1
+	return 0
+
 def get_merlin_nodeinfo(query_handler):
 	qh = nagios_qh.nagios_qh(query_handler)
-	return qh.get('#merlin nodeinfo\0')
+	ninfo = []
+	for info in qh.get('#merlin nodeinfo\0'):
+		ninfo.append(info)
+	ninfo.sort(nodeinfo_sorter)
+	return ninfo
