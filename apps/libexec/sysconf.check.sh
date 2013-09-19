957d113b8e90693dd7ff94a0c408c012ad4bab0a
Philip Eklöf <peklof@op5.com>
2013-09-19 10:04:47 +0200

    mon sysconf check: Introducing the new syscheck framework
    A simple way of quickly getting to know if something seems to
    be wrong in a Monitor system is very much needed.
    
    "mon sysconf check" has previously checked for a couple of
    ordinary things in the system that might be troublesome. These
    builtin checks were pretty static. The former version was also
    made with "information gathering" in mind. As things have
    progressed, it seems like instead, #6918 will deal with all
    sorts of system data collection.
    
    This new version makes it easier to extend the number of
    checks it runs. To make it modular, the script will execute
    check plugins, very much like the ordinary Nagios ones. The
    only difference is that these plugins will be found within
    the mon command itself, in a category called "syscheck".
    
    Any sub command added to this mon category will be treated as
    a check plugin. All such sub commands will be executed when
    "mon sysconf check" is run, and the output will end up as a
    sumarry of check results. The check plugins must not require
    any arguments to do whatever they are supposed to do.
    
    The ordinary plugin standard is maintained; exit codes, output
    formats, etc. This makes it easy for sysconf-check to find and
    show only the things that are *not* OK.
    
    Obviously, this means that the checks commands can be run
    manually, one by one, like "mon syscheck process_rrdcached".
    In other words; easily added to the ordinary monitoring, if
    one would like (as long as no root perms are required ofc.).
    
    Examples... when everything seems fine:
    
    OK: No system problems detected.
    
    ... and when it doesn't:
    
    CRITICAL: System problems detected.
    
    (db_mysql_check) CRITICAL: Error(s) detected.
    (proc_npcd) CRITICAL: PID (23799) read from lock file does not exist.
    
    When it shows failing checks, it will sometimes simply be
    needed to run the specific check plugins manually, one by
    one, to find the actual cause of the issues.
    
    Signed-off-by: Philip Eklöf <peklof@op5.com>

--- apps/libexec/sysconf.check.sh
+++ apps/libexec/sysconf.check.sh
@@ -1,149 +1,83 @@
 #!/bin/bash
 
-# bootstrapping
+# (un)set sane defaults
+unset TZ
+IFS=$' \t\n'
+LANG='C'
 
-errors=0
-verbose=0 # verbose >= 2 means echoing all commands passed to "ok" ("info" commands are always verbose)
-# verbose = 0 still shows errors, should really use "exit 1" but the "mon"-wrapper doesn't let us
+# show help text, especially for mon command
+if [ "$1" == '--help' -o "$1" == '-h' ]; then
+  printf '[-c|--complete]\n'
+  printf 'Summarizes the results of all "mon syscheck" handlers.\n'
+  printf 'The -c switch will output all check results, regardless of state.\n'
 
-# Default services/commands/alias to use
-http_service="httpd"
-mysql_service="mysqld"
-
-# Stupid check if we are on a sles server, use sles versions of those commands instead.
-# XXX: todo: enchance this check.
-if [ -e /etc/zypp ]; then
-	http_service="apache2"
-	mysql_service="mysql"
+  exit 0
 fi
 
-function usage {
-	cat <<CHECK
-[-v] [-v]
-A system diagnostics utility.
+# did the user ask to see all check results?
+complete='no'
+if [ "$1" == '--complete' -o "$1" == '-c' ]; then
+  complete='yes'
+fi
 
-Additional verbose flags (-v) increases output verbosity.
-CHECK
-}
+# fetch output of mon
+if ! handlers="$(mon --help 2>&1)"; then
+  printf 'UNKNOWN: Failed running "mon".\n\n%s\n' "$handlers"
+  exit 3
+fi
 
-while :; do
-	case "$1" in
-		-v|--verbose)
-			verbose=`expr $verbose + 1`
-			;;
-		-h|--help|help)
-			usage
-			exit
-			;;
-		*)
-			break
-			;;
-	esac
-	shift
+# find all submods
+handlers="$(printf '%s\n' "$handlers" | \
+  grep '^[[:space:]]*syscheck[[:space:]]*:' | cut -d: -f2 | sed 's/,/ /g')"
+
+# make sure we actually found some sub commands
+run='no'
+for handler in $handlers; do
+  run='yes'
+  break
+done
+if [ "$run" != 'yes' ]; then
+  printf 'UNKNOWN: No mon syscheck sub commands found.\n'
+  exit 3
+fi
+
+# sane defaults
+main_out=''
+main_ret='0'
+
+for handler in $handlers; do
+  # run handler - not interesting if ok
+  handler_out="$(mon syscheck $handler 2>&1)"
+
+  # fetch handler exit code
+  handler_ret="$?"
+
+  [ "$handler_ret" == '0' -a "$complete" == 'no' ] && continue
+
+  # increase main exit code if handler's code was more severe
+  [ "$handler_ret" -gt "$main_ret" ] && main_ret="$handler_ret"
+
+  # get first line of handler output
+  read -r line <<< "$handler_out"
+
+  # append to output: handler name + first line of handler output + newline
+  main_out+="($handler) $line"$'\n'
 done
 
-function category {
-	test $verbose -ne 0 && echo "=== $1 ==="
-}
 
-function ok {
-	# call with either:
-	# ok "echo yo" "could echo yo"
-	# ok ! "xcak" "command xcak should not exist"
-	local cmd="$1"
-	local invert=0
-	if [ "$cmd" = "!" ]; then
-		invert=1
-		shift
-		cmd="$1"
-	fi
-	local explanation="$2"
-	if [ $verbose -gt 1 ]; then
-		eval $cmd
-	else
-		eval $cmd &> /dev/null
-	fi
-	local status=$?
-	if [ $verbose -gt 0 ]; then
-		echo -n "$cmd ... "
-	fi
-	if [ $invert -eq 1 ]; then
-		if [ $status -eq 0 ]; then
-			status=1
-		else
-			status=0
-		fi
-	fi
-	if [ $status -ne 0 ]; then
-		errors=`expr $errors + 1`
-		if [ $verbose -gt 0 ]; then
-			echo "FAIL"
-			echo -e "\t$explanation"
-		fi
-	elif [ $verbose -gt 0 ]; then
-		echo "OK"
-	fi
-}
+# print final exit status msg and exit...
 
-function info {
-	# like ok, but ignores errors
-	# needs -v -v to output anything
-	if [ $verbose -lt 2 ]; then
-		return
-	fi
-	local cmd="$1"
-	local explanation="$2"
-	echo "$cmd ... INFO"
-	test -n "$explanation" && echo "$explanation"
-	eval $cmd
-}
+if [ "$main_ret" == '0' ]; then
+  printf 'OK: No system problems detected.\n' "$"
+else
+  case "$main_ret" in
+    1) prefix='WARNING' ;;
+    2) prefix='CRITICAL' ;;
+    3) prefix='UNKNOWN' ;;
+    *) prefix="UNKNOWN ($main_ret)" main_ret='3' ;;
+  esac
 
-function show_results {
-	test $verbose -ne 0 && echo
-	if [ $errors -ne 0 ]; then
-		echo "$errors errors found, system is not production ready"
-		exit 3
-	fi
-	echo "System is production ready"
-}
+  printf '%s: System problems detected.\n\n%s' "$prefix" "$main_out"
+fi
 
-function abort_if_not_root {
-	if [ $UID -ne 0 ]; then
-		echo "You must be root to run this script. Try this:"
-		echo "sudo mon sysconf check"
-		exit 3
-	fi
-}
-
-# actual program:
-
-abort_if_not_root
-
-category "USERS"
-ok "cat /etc/passwd | grep monitor" "monitor user should exist"
-
-category "FILES, FOLDERS & RIGHTS"
-ok "cat /etc/op5-monitor-release"
-ok "ls -la /etc/op5" "Files should belong to monitor:apache"
-info "ls -laR /opt/monitor/etc" "Files should belong to monitor:apache"
-info "ls -la /opt/monitor/core*"
-
-category "PROCESSES"
-ok "service monitor status" "Try mon start"
-ok "service rrdcached status"
-ok "service $http_service status"
-ok "service $mysql_service status"
-ok "ps -ef | grep synergy"
-ok "pidof merlind"
-ok "mon node status"
-
-category "SYSTEM"
-info "uname -a"
-info "head -n 999999 /etc/*release*"
-info "date"
-info "rpm -qa"
-info "ls -la /opt/plugins"
-info "head -n 999999 /etc/cron.d/*"
-info "hostname"
-
-show_results
+exit "$main_ret"
