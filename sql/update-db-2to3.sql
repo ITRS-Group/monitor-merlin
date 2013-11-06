cb67a437587195e9e869a4cd9431e6df7f318d52
Anton Lofgren <alofgren@op5.com>
2013-11-06 13:26:19 +0100

    update-db-2to3: add long_output to report_data_extras
    The report_data_extras schema should mirror the report_data one (except
    for the id).
    
    This patch also fixes a syntax error in the creation of the db version
    procedure. Apparently, 'set_initial_db_version' is not the same as
    set_initial_db_version (without the quotes). Thus, we've been trying to
    drop the wrong procedure. That's bad, because then we can't create it
    again, because it already exists.
    
    Change-Id: I0e5a74f497d09ec09aafe5300366efeee82adef8
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

--- sql/update-db-2to3.sql
+++ sql/update-db-2to3.sql
@@ -25,3 +25,31 @@ FROM report_data;
 ALTER TABLE report_data RENAME report_data_old;
 ALTER TABLE report_data_tmp RENAME report_data;
 DROP TABLE report_data_old;
+
+--- No id added for report_data_extras, only long_output
+CREATE TABLE report_data_extras_tmp (
+	`timestamp` int(11) NOT NULL DEFAULT '0',
+	`event_type` int(11) NOT NULL DEFAULT '0',
+	`flags` int(11) DEFAULT NULL,
+	`attrib` int(11) DEFAULT NULL,
+	`host_name` varchar(255) COLLATE latin1_general_cs DEFAULT '',
+	`service_description` varchar(255) COLLATE latin1_general_cs DEFAULT '',
+	`state` int(2) NOT NULL DEFAULT '0',
+	`hard` int(2) NOT NULL DEFAULT '0',
+	`retry` int(5) NOT NULL DEFAULT '0',
+	`downtime_depth` int(11) DEFAULT NULL,
+	`output` text COLLATE latin1_general_cs,
+	`long_output` text COLLATE latin1_general_cs,
+	KEY `rd_timestamp` (`timestamp`),
+	KEY `rd_event_type` (`event_type`),
+	KEY `rd_name_evt_time` (`host_name`,`service_description`,`event_type`,`hard`,`timestamp`),
+	KEY `rd_state` (`state`)
+);
+
+INSERT INTO report_data_extras_tmp (`timestamp`, `event_type`, `flags`, `attrib`, `host_name`, `service_description`, `state`, `hard`, `retry`, `downtime_depth`, `output`)
+SELECT `timestamp`, `event_type`, `flags`, `attrib`, `host_name`, `service_description`, `state`, `hard`, `retry`, `downtime_depth`, `output`
+FROM report_data_extras;
+
+ALTER TABLE report_data_extras RENAME report_data_extras_old;
+ALTER TABLE report_data_extras_tmp RENAME report_data_extras;
+DROP TABLE report_data_extras_old;
