003b0bd10a4de01c1b88c3c88422793e1bec8110
Robin Sonefors <robin.sonefors@op5.com>
2014-01-24 15:49:58 +0100

    apps: Add new "mon sysconf rrdmultiple"
    This means that putting PNP in multiple mode is as easy as
     mon sysconf rrdmultiple
    which means it's easy enough to do for existing installations, and
    already the default for new installations, so that resolves bug #6544.
    
    Change-Id: Ie8fbbed894f93f1d30f095feded6dd3b7ed00b12
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

---
+++ apps/libexec/sysconf.rrdmultiple.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+set -e
+
+if [ "$UID" -gt 0 ]; then
+	echo "You need to run this as root"
+	exit 1;
+fi
+
+service npcd stop || :
+service rrdcached stop || :
+pushd /opt/monitor/op5/pnp
+chown -R monitor:apache perfdata
+tar czf perfdata-backup.tar.gz perfdata
+service rrdcached start
+su monitor -c 'for check in $(./rrd_convert.pl --cfg_dir=/opt/monitor/etc/pnp --list_commands|awk "{print \$2 }"); do yes|./rrd_convert.pl --cfg_dir=/opt/monitor/etc/pnp --check_command=$check --no_structure_check;done'
+sed -i -e 's/RRD_STORAGE_TYPE = SINGLE/RRD_STORAGE_TYPE = MULTIPLE/' /opt/monitor/etc/pnp/process_perfdata.cfg
+find perfdata -name *.xml -exec rm {} \;
+tar tvzf perfdata-backup.tar.gz |grep rrd|awk '{print $6}'|xargs rm
+popd
+service npcd start
+service rrdcached start
