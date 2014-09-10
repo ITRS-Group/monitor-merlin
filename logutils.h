b463ff99a78c73fa18f468e52e1ab36de36abf5f
Robin Sonefors <robin.sonefors@op5.com>
2014-09-10 13:09:09 +0200

    logutils: Only keep two copies of all ascii colors
    One in python, and one in C remains. The redundant C one is hereby
    removed.
    
    Change-Id: I42761b774b8944b43bad718fc77933f63f948cf8
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- logutils.h
+++ logutils.h
@@ -13,22 +13,7 @@
 #include <nagios/nebcallbacks.h>
 #include <nagios/lib/dkhash.h>
 #include "shared.h"
-
-#define CLR_RESET          "\033[m"
-#define CLR_BOLD           "\033[1m"
-#define CLR_RED            "\033[31m"
-#define CLR_GREEN          "\033[32m"
-#define CLR_BROWN          "\033[33m"
-#define CLR_YELLOW         "\033[33m\033[1m"
-#define CLR_BLUE           "\033[34m"
-#define CLR_MAGENTA        "\033[35m"
-#define CLR_CYAN           "\033[36m"
-#define CLR_BG_RED         "\033[41m"
-#define CLR_BRIGHT_RED     "\033[31m\033[1m"
-#define CLR_BRIGHT_GREEN   "\033[32m\033[1m"
-#define CLR_BRIGHT_BLUE    "\033[34m\033[1m"
-#define CLR_BRIGHT_MAGENTA "\033[35m\033[1m"
-#define CLR_BRIGHT_CYAN    "\033[36m\033[1m"
+#include "colors.h"
 
 /* for the string code structs */
 #define add_code(n, s, c) { n, s, sizeof(s) - 1, c, }
