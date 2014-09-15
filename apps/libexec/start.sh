5d81f1304620d85148204e30cf9bfee7aaec8745
Robin Sonefors <robin.sonefors@op5.com>
2014-09-15 17:44:37 +0200

    mon: Remove deprecated crap from start, stop
    We don't support sun no more, and we really should stop slaying monitor
    when it can just as well shutdown by itself.
    
    Change-Id: I8aa57d04495ae715cc17ae9c3ceb5291505af9be
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- apps/libexec/start.sh
+++ apps/libexec/start.sh
@@ -1,27 +1,13 @@
 #!/bin/sh
 
-if [ "`uname`" = "SunOS" ]; then
-	grep=/usr/xpg4/bin/grep
-	gsed=/opt/csw/bin/gsed
-	if mon node show | $grep -q "^TYPE=master"; then
-		# we have masters, so we're a poller
-		# Disable synergy nagios stuff (fugly workaround)
-		ncfg=/opt/monitor/etc/nagios.cfg
-		test -f $ncfg && $gsed -i '#^cfg_dir=/opt/monitor/etc/synergy#d' $ncfg
-	fi
-
-	svcadm enable -ts op5merlin
-	svcadm enable -ts op5monitor
-else
-	if mon node show | grep -q ^TYPE=master; then
-		# we have masters, so we're a poller
-		# Disable synergy nagios stuff (fugly workaround) (but run sed only if needed)
-		ncfg=/opt/monitor/etc/nagios.cfg
-		test -f $ncfg && \
-			grep -q '^cfg_dir=/opt/monitor/etc/synergy' $ncfg && \
-			sed -i '/^cfg_dir=\/opt\/monitor\/etc\/synergy/d' $ncfg
-	fi
-
-	/etc/init.d/merlind start
-	/etc/init.d/monitor start
+if mon node show | grep -q ^TYPE=master; then
+	# we have masters, so we're a poller
+	# Disable synergy nagios stuff (fugly workaround) (but run sed only if needed)
+	ncfg=/opt/monitor/etc/nagios.cfg
+	test -f $ncfg && \
+		grep -q '^cfg_dir=/opt/monitor/etc/synergy' $ncfg && \
+		sed -i '/^cfg_dir=\/opt\/monitor\/etc\/synergy/d' $ncfg
 fi
+
+/etc/init.d/merlind start
+/etc/init.d/monitor start
--- apps/libexec/stop.sh
+++ apps/libexec/stop.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-if [ "`uname`" = "SunOS" ]; then
-	/opt/monitor/svc/svc-monitor slay
-	svcadm disable -t op5monitor
-	svcadm disable -ts op5merlin
-else
-	/etc/init.d/monitor stop
-	/etc/init.d/monitor slay
-	/etc/init.d/merlind stop
-fi
+/etc/init.d/monitor stop
+/etc/init.d/merlind stop
