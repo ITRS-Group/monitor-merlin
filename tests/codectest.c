872b42b6e96838be39cbfc5c714fc8e0fe33b322
Robin Sonefors <robin.sonefors@op5.com>
2014-11-25 15:44:05 +0100

    test: Add test for codec
    The focus is on testing the handling of too long strings, as tracked
    by bug #9484.
    
    Change-Id: Ia796f22795e33c06660dcdafd370c91c7a67cf44
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

---
+++ tests/codectest.c
@@ -0,0 +1,99 @@
+#include "codec.h"
+#include "logging.c"
+#include <check.h>
+
+void general_setup()
+{
+	merlin_log_file = "stdout";
+	log_levels = -1;
+	log_init();
+}
+
+void general_teardown()
+{
+}
+
+START_TEST(test_encode_serviceevent)
+{
+	int ret;
+	merlin_event pkt;
+	memset(&pkt, 0, sizeof(pkt));
+	pkt.hdr.type = NEBCALLBACK_SERVICE_CHECK_DATA;
+	pkt.hdr.selection = DEST_BROADCAST;
+	pkt.hdr.code = MAGIC_NONET;
+	merlin_service_status ds;
+	memset(&ds, 0, sizeof(ds));
+	merlin_service_status *out;
+	/* set some relevant values at the beginning and end of the struct */
+	ds.state.initial_state = 123;
+	ds.state.notified_on = 456;
+	ds.host_name = "foo";
+	ds.state.perf_data = "bar";
+	ret = merlin_encode_event(&pkt, (void *)&ds);
+	ck_assert_int_gt(ret, 0);
+	pkt.hdr.len = ret;
+	ret = merlin_decode_event(NULL, &pkt);
+	ck_assert_int_eq(ret, 0);
+	out = (merlin_service_status *) pkt.body;
+	ck_assert_int_eq(123, out->state.initial_state);
+	ck_assert_int_eq(456, out->state.notified_on);
+	ck_assert_str_eq("foo", out->host_name);
+	ck_assert_str_eq("bar", out->state.perf_data);
+}
+END_TEST
+
+START_TEST(test_encode_too_long)
+{
+	int ret;
+	merlin_event pkt;
+	memset(&pkt, 0, sizeof(pkt));
+	pkt.hdr.type = NEBCALLBACK_SERVICE_CHECK_DATA;
+	pkt.hdr.selection = DEST_BROADCAST;
+	pkt.hdr.code = MAGIC_NONET;
+	merlin_service_status ds;
+	memset(&ds, 0, sizeof(ds));
+	merlin_service_status *out;
+	char *input = calloc((1 << 20) + 1, 1);
+	memset(input, 'a', 1 << 20);
+	input[1<<20] = 0;
+	ds.host_name = strdup(input);
+	ds.service_description = "This should be truncated away";
+	ds.state.perf_data = "This should be truncated away";
+	ds.state.plugin_output = "This should be truncated away";
+	ds.state.long_plugin_output = "This should be truncated away";
+	ret = merlin_encode_event(&pkt, (void *)&ds);
+	ck_assert_int_gt(ret, 0);
+	pkt.hdr.len = ret;
+	ret = merlin_decode_event(NULL, &pkt);
+	ck_assert_int_eq(ret, 0);
+	out = (merlin_service_status *) pkt.body;
+	ck_assert(pkt.body[(128 << 10) - 1] == 0);
+	ck_assert(strcmp(input, out->host_name));
+	ck_assert(!strncmp(input, out->host_name, 120<<10));
+	ck_assert_ptr_eq(NULL, out->service_description);
+}
+END_TEST
+
+Suite *
+check_codec_suite(void)
+{
+	Suite *s = suite_create("codec");
+
+	TCase *tc = tcase_create("codec");
+	tcase_add_checked_fixture (tc, general_setup, general_teardown);
+	tcase_add_test(tc, test_encode_serviceevent);
+	tcase_add_test(tc, test_encode_too_long);
+	suite_add_tcase(s, tc);
+
+	return s;
+}
+
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[]) {
+		int number_failed;
+	Suite *s = check_codec_suite();
+	SRunner *sr = srunner_create(s);
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
