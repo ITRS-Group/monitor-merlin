11f56cfbe5eca027123a1f612cc4b9e17da6dbcc
Mattias Ryrlén <mattiasr@op5.com>
2014-12-12 15:07:37 +0100

    test.py: show SINGALS_TO_NAME_DICT result as string
    As the name says, we convert the signal to string, hence it's not a int.
    
    Change-Id: Ide3b4169f118ae39421bf36e7f2531ac0a8eefd2
    Signed-off-by: Mattias Ryrlén <mattiasr@op5.com>

--- apps/libexec/test.py
+++ apps/libexec/test.py
@@ -578,7 +578,7 @@ class fake_mesh:
 				clr = color.green
 			else:
 				clr = color.bright_red
-			msg = "%s: %sGot sig %d%s" % (msg, clr, SIGNALS_TO_NAMES_DICT.get(os.WTERMSIG(status), os.WTERMSIG(status)), color.reset)
+			msg = "%s: %sGot sig %s%s" % (msg, clr, SIGNALS_TO_NAMES_DICT.get(os.WTERMSIG(status), os.WTERMSIG(status)), color.reset)
 			if os.WCOREDUMP(status):
 				msg = "%s (%score dumped%s)" % (msg, color.bright_red, color.reset)
 				return sub.fail(msg)
