5ce3f5b8952838a48fca577fbd630c0264576877
Mattias Ryrlén <mattiasr@op5.com>
2016-08-24 08:59:29 +0200

    libexec/log.py: Added testcases for more than 10 years old files
    To see that we can purge them.
    
    Signed-off-by: Mattias Ryrlén <mattiasr@op5.com>

--- tests/pyunit/test.py
+++ tests/pyunit/test.py
@@ -9,6 +9,37 @@ import shutil
 sys.path.append(os.path.abspath('apps/libexec'))
 import log
 
+class PurgeLogTestMoreThan10YearsOldCase(unittest.TestCase):
+	def setUp(self):
+		# Create some files in temporary archive_dir
+		log.archive_dir = "./TestPurgeLogs%s" % next(tempfile._get_candidate_names())
+		os.mkdir(log.archive_dir)
+
+		files = [
+			"naemon.log-19900505",
+			"naemon.log-20000824",
+			"nagios-08-24-2000-14.log",
+			"nagios-07-21-1992-14.log",
+		]
+		for file in files:
+			with open(os.path.join(log.archive_dir, file), 'wb') as temp_file:
+				temp_file.write('Testing file')
+
+	def tearDown(self):
+		# Remove the temporary archive_dir
+		shutil.rmtree(log.archive_dir)
+
+	def testPurge_log_files(self):
+		import glob
+		import time
+		expected_result = []
+		oldest = time.mktime(time.gmtime(1469048400)) #  2016-07-20T21:00:00+00:00
+		log.purge_naemon_log_files(oldest)
+		actual_result = glob.glob("%s/*" % log.archive_dir)
+
+		# Compare number of files with the expected result
+		self.assertEqual(set(expected_result), set(actual_result))
+
 class PurgeLogTestCase(unittest.TestCase):
 	def setUp(self):
 		# Create some files in temporary archive_dir
