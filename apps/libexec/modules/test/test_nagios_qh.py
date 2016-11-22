cfadb8c18300f213c3633952d73cadf2a4107ad3
Tomas Vestelind <tvestelind@op5.com>
2016-11-22 10:15:04 +0100

    more unit tests for nagios_qhs format
    I misunderstood the interface a bit. I rewrote the unittests to reflect how I
    think that the API should be used.
    
    Signed-off-by: Tomas Vestelind <tvestelind@op5.com>

--- apps/libexec/modules/test/test_nagios_qh.py
+++ apps/libexec/modules/test/test_nagios_qh.py
@@ -1,37 +1,55 @@
 from nagios_qh import nagios_qh
 
 
-def test_format_one_value():
+def test_format_one_block_one_row_one_value():
     n = nagios_qh(None)
-    result = list(n.format("test=1"))
+    result = list(n.format(["test=1"]))
     assert result == [{"test": "1"}], "Unexpected result %s" % result
 
 
-def test_format_two_values_one_row():
+def test_format_one_block_one_row_two_values():
     n = nagios_qh(None)
-    result = list(n.format("test=1;test2=13"))
+    result = list(n.format(["test=1;test2=13"]))
     assert result == [{"test": "1", "test2": "13"}], "Unexpected result %s" % result
 
 
-def test_format_no_values_one_row():
+def test_format_no_block():
     n = nagios_qh(None)
-    result = list(n.format(""))
+    result = list(n.format([]))
     assert result == [], "Unexpected result %s" % result
 
 
-def test_format_two_values_two_rows():
+def test_format_one_block_two_rows_two_values():
     n = nagios_qh(None)
-    result = list(n.format("test=1\ntest2=13"))
+    result = list(n.format(["test=1\ntest2=13"]))
     assert result == [{"test": "1"}, {"test2": "13"}], "Unexpected result %s" % result
 
 
-def test_format_three_values_two_rows():
+def test_format_one_block_two_rows_three_values():
     n = nagios_qh(None)
-    result = list(n.format("test=1\ntest2=13;test3=42"))
+    result = list(n.format(["test=1\ntest2=13;test3=42"]))
     assert result == [{"test": "1"}, {"test2": "13", "test3": "42"}], "Unexpected result %s" % result
 
 
-def test_format_two_values_three_rows():
+def test_format_one_block_three_rows_two_values():
     n = nagios_qh(None)
-    result = list(n.format("test=1\n\ntest2=13"))
+    result = list(n.format(["test=1\n\ntest2=13"]))
     assert result == [{"test": "1"}, {"test2": "13"}], "Unexpected result %s" % result
+
+
+def test_format_two_blocks_one_row_one_value():
+    n = nagios_qh(None)
+    result = list(n.format(["test", "=1"]))
+    assert result == [{"test": "1"}], "Unexpected result %s" % result
+
+
+def test_format_two_blocks_one_row_two_values():
+    n = nagios_qh(None)
+    result = list(n.format(["test", "=1", ";test2=2"]))
+    assert result == [{"test": "1", "test2": "2"}], "Unexpected result %s" % result
+
+
+def test_format_two_blocks_two_rows_two_values():
+    n = nagios_qh(None)
+    result = list(n.format(["test=1", "\ntest2=2"]))
+    assert result == [{"test": "1"}, {"test2": "2"}], "Unexpected result %s" % result
