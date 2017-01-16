1c483eb68090a06fa09bcc4870902ccb6b165c6c
Tomas Vestelind <tvestelind@op5.com>
2017-01-16 14:20:52 +0100

    Run naemon's config check as non-root in mon restart
    naemon can no longer be executed as root so we execute it as the configured
    naemon user.
    
    Signed-off-by: Tomas Vestelind <tvestelind@op5.com>

--- apps/libexec/restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if /usr/bin/naemon  --precache-objects --verify-config /opt/monitor/etc/naemon.cfg; then
-	mon stop
-	mon start
-else
-	echo "Refusing to restart monitor with a flawed configuration"
-	exit 1
-fi
