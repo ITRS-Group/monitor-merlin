794a89e81839e48e470802bfdf05adb864e11289
Jacob Hansen <jhansen@op5.com>
2018-08-21 09:43:21 +0000

    Tests: Check that systemd starts merlind as non-root
    Part of: MON-11122
    
    Signed-off-by: Jacob Hansen <jhansen@op5.com>

---
+++ features/step_definitions/systemd_merlind.rb
@@ -0,0 +1,17 @@
+# must be run as root with merlind installed
+Given(/^I start merlind with systemd$/) do
+    ret = system ( "systemctl start merlind" )
+		if (ret == false)
+        fail("Failed to start merlind with systemd")
+    end
+end
+
+
+Then(/^merlind should not run as the root user$/) do
+    # verify merlind is not running as root user
+    # also fails if merlind is not running at all
+    ret = system ("ps aux | grep merlind | grep -v grep | grep -v root")
+		if (ret == false)
+        fail("Merlind runs as root user or not at all")
+    end
+end
