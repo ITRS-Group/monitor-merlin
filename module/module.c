fed967970279f5f1e725959c765edb548f13336a
Jerick Macario <jmacario@itrsgroup.com>
2024-07-22 10:37:06 +0800

    Updated to use new delete comments functions
    Changed old logic to delete comments using updated calls from merlin-naemon.
    
    Part of MON-13296.
    
    Signed-of-by: Jerick Macario <jmacario@itrsgroup.com>

--- module/module.c
+++ module/module.c
@@ -615,40 +615,6 @@ static int handle_external_command(merlin_node *node, void *buf)
 	process_external_command2(ds->command_type, ds->entry_time, ds->command_args);
 	return 1;
 }
-
-static int matching_comment(comment *cmnt, nebstruct_comment_data *ds)
-{
-	/*
-	 * hash collisions can cause comments from other objects
-	 * (and other types of objects) to be listed here, so we
-	 * must match on host_name and service_description as
-	 * well and skip comments that don't match the type
-	 */
-	if (cmnt->comment_type == ds->comment_type &&
-		cmnt->entry_type == ds->entry_type &&
-		cmnt->source == ds->source &&
-		cmnt->expires == ds->expires &&
-		cmnt->expire_time == ds->expire_time &&
-		cmnt->entry_time == ds->entry_time &&
-		cmnt->persistent == ds->persistent &&
-		!strcmp(cmnt->author, ds->author_name) &&
-		!strcmp(cmnt->comment_data, ds->comment_data) &&
-		!strcmp(cmnt->host_name, ds->host_name) &&
-		(cmnt->service_description == ds->service_description ||
-		 !strcmp(cmnt->service_description, ds->service_description)))
-	{
-		ldebug("CMNT: cmnt->host_name: %s; ds->host_name: %s",
-			   cmnt->host_name, ds->host_name);
-		ldebug("CMNT: cmnt->author: %s; ds->author_name: %s",
-			   cmnt->author, ds->author_name);
-		ldebug("CMNT: cmnt->comment_data: %s; ds->comment_data: %s",
-			   cmnt->comment_data, ds->comment_data);
-		return 1;
-	}
-
-	return 0;
-}
-
 static int handle_comment_data(merlin_node *node, merlin_header *hdr, void *buf)
 {
 	nebstruct_comment_data *ds = (nebstruct_comment_data *)buf;
@@ -673,29 +639,12 @@ static int handle_comment_data(merlin_node *node, merlin_header *hdr, void *buf)
 	}
 
 	if (ds->type == NEBTYPE_COMMENT_DELETE) {
-		comment *cmnt, *next_cmnt;
+		host *hs = find_host(ds->host_name);
 
 		if (ds->comment_type == HOST_COMMENT) {
-			cmnt = get_first_comment_by_host(ds->host_name);
-			for (; cmnt; cmnt = next_cmnt) {
-				next_cmnt = cmnt->next;
-				if (matching_comment(cmnt, ds)) {
-					merlin_set_block_comment(ds);
-					delete_comment(cmnt->comment_type, cmnt->comment_id);
-					merlin_set_block_comment(NULL);
-				}
-			}
+			delete_all_host_comments(hs);
 		} else {
-			/* this is *really* expensive. Sort of wtf? */
-			for (cmnt = comment_hashtable; cmnt; cmnt = next_cmnt) {
-				next_cmnt = cmnt->next;
-
-				if (matching_comment(cmnt, ds)) {
-					merlin_set_block_comment(ds);
-					delete_comment(cmnt->comment_type, cmnt->comment_id);
-					merlin_set_block_comment(NULL);
-				}
-			}
+			delete_all_service_comments(hs);
 		}
 		return 0;
 	} else {
@@ -852,7 +801,7 @@ int handle_event(merlin_node *node, merlin_event *pkt)
 	/* let the various handler know which node sent the packet */
 	pkt->hdr.selection = node->id;
 
-	if (!node->state == STATE_CONNECTED) {
+	if (!(node->state == STATE_CONNECTED)) {
 		lwarn("STATE: Discarding %s event from %s %s",
 			  callback_name(pkt->hdr.type), node_type(node), node->name);
 		return 0;
